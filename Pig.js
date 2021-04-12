8917f4db4b9e2effc55289cfda08914b6f242d33
KrishKrishb <79356557+KrishKrishb@users.noreply.github.com>
2021-04-12 15:50:42 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,8 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+     super(x,y,50,50);//super is used for transferring the all the properties of the parent class to child class
+     this.image=loadImage("sprites/enemy.png");
+       
+      }
+     
+    }
